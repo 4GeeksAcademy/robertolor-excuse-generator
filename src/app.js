d124c82c22adf3c774281a5a43fbfe981cb56ab1
Roberto <51332297+Robertolor@users.noreply.github.com>
2024-03-16 00:49:46 +0000

    movi el codigo de otro codespace que tenia que no me dejaba pushear

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuseGenerator = () => {
+    let excuse = [];
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    excuse.push(who[Math.floor(Math.random() * (who.length - 1))]);
+    excuse.push(" " + action[Math.floor(Math.random() * (action.length - 1))]);
+    excuse.push(" " + what[Math.floor(Math.random() * (what.length - 1))]);
+    excuse.push(" " + when[Math.floor(Math.random() * (when.length - 1))]);
+    return excuse.join("");
+  };
+  let texto = excuseGenerator();
+  document.getElementById("excuse").innerHTML = texto;
+  console.log(onload);
 };
